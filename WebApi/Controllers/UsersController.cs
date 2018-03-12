84059fa4ceefd10bdcf4f3b1d082e346b67b1168
Denis Sokolov <deniskin@live.ru>
2018-03-13 01:05:48 +0300

    JWT Authentication

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+	[Authorize]
+	[Route("api/users")]
+	public class UsersController : Controller
+	{
+		[Route("me")]
+		public IActionResult Get()
+		{
+			string id = User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+			return Ok(id);
+		}
+	}
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						ValidIssuer = "yourdomain.com",
+						ValidAudience = "yourdomain.com",
+						IssuerSigningKey = new SymmetricSecurityKey(
+							Encoding.UTF8.GetBytes("У попа была собака."))
+					};
+				});
+
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseAuthentication();
+
+			app.UseMvc();
+		}
+	}
+}
